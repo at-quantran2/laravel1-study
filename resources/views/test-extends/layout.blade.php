bb29125a4026fc823fd4d1ddc279516362d3690b
at-quantran2 <quan.tran2@asiantech.vn>
2018-07-08 23:04:49 +0700

    Blade 2 condition check isset

--- resources/views/test-extends/layout.blade.php
+++ resources/views/test-extends/layout.blade.php
@@ -9,5 +9,23 @@
 @stop
 @section('content')
     Noi dung trang layout
+    <?php
+    $name = '<b>Tran Hong Quan</b>';
+    $age = 16;
+    ?>
+    <br>
+    {{$name}} 
+    <br>
+    {!!$name!!}
+    <br>
+    @if ($age  < 10)
+        Diem < 10
+    @elseif ($age < 15)
+      10 < Diem < 15
+    @else
+        Diem >= 15
+    @endif
+    <br>
+    {{ $nam1e or 'Error' }}
 @stop
 <h2></h2>
