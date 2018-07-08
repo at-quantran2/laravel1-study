353bda7eabcfe5931b2545fa67e37111b1eb079a
at-quantran2 <quan.tran2@asiantech.vn>
2018-07-08 16:57:10 +0700

    Bai 4 route

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,21 @@ Route::get('/top', function() {
     echo 'View for testroute123';
 });
 Route::get('thongtin','Controller@showinfor') ;
+Route::get('courses/{sub}/{time}',function($subject, $time) {
+    echo "This is Course $subject start at $time";
+});
+Route::get('courses/{sub?}',function($subject = 'default subject') {
+    return " Course $subject";
+});
+
+Route::get('people/{name}/{age}',function($name, $age) {
+    return "Hello $name is $age";
+})->where(['name' => '[a-zA-Z]+', 'age' => '[0-9]{5}']);
+
+Route::get('user/{id}', function ($id) {
+    return "User $id";
+})->where('id', '[0-9]+');
+
+Route::get('user/{id}/{name}', function ($id, $name) {
+    //
+})->where(['id' => '[0-9]+', 'name' => '[a-z]+']);
