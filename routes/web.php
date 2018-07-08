6298d13e150d90f26376338cb58cf9ea515d9f1d
at-quantran2 <quan.tran2@asiantech.vn>
2018-07-08 16:19:22 +0700

    bai3 MVC

---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/testroute', function() {
+    echo 'View for testroute';
+});
+Route::get('/top', function() {
+    echo 'View for testroute123';
+});
+Route::get('thongtin','Controller@showinfor') ;
