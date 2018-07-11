4e3fd9f52284ccf4bc82e30e5de0015b79fdc419
at-quantran2 <quan.tran2@asiantech.vn>
2018-07-11 08:51:53 +0700

    Bai 20 Tao du lieu mau seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call(ProductTableSeeder::class);
+    }
+}
+class ProductTableSeeder extends Seeder
+{
+    public function run()
+    {
+        DB::table('product')->insert(
+            [['name' => 'item11', 'price' => 2200, 'cate_id' => 1],
+            ['name' => 'item22', 'price' => 2300, 'cate_id' => 2],
+            ['name' => 'item33', 'price' => 2400, 'cate_id' => 1],
+            ['name' => 'item44', 'price' => 2500, 'cate_id' => 1],
+            ['name' => 'item55', 'price' => 2600, 'cate_id' => 2],
+            ['name' => 'item66', 'price' => 2700, 'cate_id' => 2]]
+        );
     }
 }
